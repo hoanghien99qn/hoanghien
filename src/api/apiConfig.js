6d0031ac60d2b30d718c9df6536994e1ce38a9be
hoanghien99qn <mystery16799@gmail.com>
2021-11-10 10:52:12 +0700

    fix mix-content issue and edit readme

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,10 +1,10 @@
 const apiConfig = {
-    baseUrl: 'https://api.themoviedb.org/3/',
+    baseUrl: '//api.themoviedb.org/3/',
     apiKey: '04c35731a5ee918f014970082a0088b1',
-    originalImage: (imgPath) => `https://www.themoviedb.org/t/p/original/${imgPath}`,
-    w220Image: (imgPath) => `https://www.themoviedb.org/t/p/w220_and_h330_face/${imgPath}`,
-    w300Image: (imgPath) => `https://www.themoviedb.org/t/p/w300/${imgPath}`,
-    w1920Image: (imgPath) => `https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces/${imgPath}`,
+    originalImage: (imgPath) => `//www.themoviedb.org/t/p/original/${imgPath}`,
+    w220Image: (imgPath) => `//www.themoviedb.org/t/p/w220_and_h330_face/${imgPath}`,
+    w300Image: (imgPath) => `//www.themoviedb.org/t/p/w300/${imgPath}`,
+    w1920Image: (imgPath) => `//www.themoviedb.org/t/p/w1920_and_h800_multi_faces/${imgPath}`,
 }
 
 export default apiConfig
